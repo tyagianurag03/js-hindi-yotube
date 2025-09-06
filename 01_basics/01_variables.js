cafc30f6ebe6b5be3d0e234c61547c7a80f43518
tyagianurag03 <officialanuragtyagi03@gmail.com>
2025-09-06 16:56:54 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -17,4 +17,4 @@ prefer not to use var
 because of issue in block and functional scope
 */
 
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
