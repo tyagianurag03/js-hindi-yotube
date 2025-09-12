a975d3ce67abf5f6491360c85499da481b2e5ce6
tyagianurag03 <officialanuragtyagi03@gmail.com>
2025-09-12 18:25:17 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+
+const bigNumber = 83473726477374565465476n
+
+// Reference Type(Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "Anurag",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello World");   
+}
+
+console.log(typeof anotherId);
