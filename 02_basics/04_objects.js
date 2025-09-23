a2dac64f4b273663429d76f5c750704bd494bb2b
tyagianurag03 <officialanuragtyagi03@gmail.com>
2025-09-23 19:25:26 +0000

    objects2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,27 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "not@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Anurag",
+            lastname: "tyagi"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {obj1,obj2}
+
+console.log(obj3)
