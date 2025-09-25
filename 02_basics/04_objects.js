35be7ffe6eb3088b5182f9ec7122b72c1193bab6
tyagianurag03 <officialanuragtyagi03@gmail.com>
2025-09-25 18:42:59 +0000

    more on objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -21,7 +21,35 @@ const regularUser = {
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = {obj1,obj2}
+// const obj3 = Object.assign({},obj1,obj2,obj4)
 
-console.log(obj3)
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "A@gmail.com"
+    },
+    {
+        id: 1,
+        email: "A@gmail.com"
+    },
+    {
+        id: 1,
+        email: "A@gmail.com"
+    },
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLogged'));
+
